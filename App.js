85ce7517ea60f6d73c6a2a8095d4af68c1b0f7e0
Evgeniya1307 <evgeniya_leonova_00@mail.ru>
2022-10-28 00:23:35 +0400

    <BottomSheetModalProvider>

--- App.js
+++ App.js
@@ -1,26 +1,138 @@
-
-import React from 'react';
-import { View, StatusBar, StyleSheet } from 'react-native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import BoottomSheet from './components/BoottomSheet';
-
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import {
+  Button,
+  Pressable,
+  StyleSheet,
+  Switch,
+  Text,
+  useWindowDimensions,
+  View,
+} from "react-native";
+import {
+  BottomSheetModal,
+  BottomSheetModalProvider,
+} from "@gorhom/bottom-sheet";
+import { useRef, useState } from "react";
+import { AntDesign } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 
 export default function App() {
+  const [darkmode, setDarkmode] = useState(false);
+  const [device, setDevice] = useState(false);
+  const { width } = useWindowDimensions();
+  const [theme, setTheme] = useState("dim");
+  const [isOpen, setIsOpen] = useState(false);
+
+  const bottomSheetModalRef = useRef(null);
+
+  const snapPoints = ["25%", "48%", "75%"];
+
+  function handlePresentModal() {
+    bottomSheetModalRef.current?.present();
+    setTimeout(() => {
+      setIsOpen(true);
+    }, 100);
+  }
+
   return (
-    <GestureHandlerRootView style={{flex:1}}>
-    <View style={styles.container}>
-     <StatusBar style ="light" />
-    <BoottomSheet/>
-    </View>
-    </GestureHandlerRootView>
+    <BottomSheetModalProvider>
+      <View
+        style={[
+          styles.container,
+          { backgroundColor: isOpen ? "gray" : "white" },
+        ]}
+      >
+        <Button title="Поиск Горничных..." onPress={handlePresentModal} />
+        <StatusBar style="auto" />
+        <BottomSheetModal
+          ref={bottomSheetModalRef}
+          index={1}
+          snapPoints={snapPoints}
+          backgroundStyle={{ borderRadius: 50 }}
+          onDismiss={() => setIsOpen(false)}
+        >
+          <View style={styles.contentContainer}>
+            <Text style={[styles.title, { marginBottom: 20 }]}>Дополнительные услуги</Text>
+            <View style={styles.row}>
+              <Text style={styles.subtitle}>Чистка пола Роторной машиной</Text>
+              <Switch
+                value={darkmode}
+                onChange={() => setDarkmode(!darkmode)}
+              />
+            </View>
+            <View style={styles.row}>
+              <Text style={styles.subtitle}>Мойка окон</Text>
+              <Switch value={device} onChange={() => setDevice(!device)} />
+            </View>
+            <Text style={styles.description}>
+            Уборка квартир, домов и офисов
+            </Text>
+            <View
+              style={{
+                width: width,
+                borderBottomWidth: StyleSheet.hairlineWidth,
+                borderBottomColor: "gray",
+                marginVertical: 30,
+              }}
+            />
+            <Text style={[styles.title, { width: "100%" }]}>Наши услуги</Text>
+            <Pressable style={styles.row} onPress={() => setTheme("dim")}>
+              <Text style={styles.subtitle}>Простая уборка</Text>
+              {theme === "dim" ? (
+                <AntDesign name="checkcircle" size={24} color="#4A98E9" />
+              ) : (
+                <Entypo name="circle" size={24} color="#56636F" />
+              )}
+            </Pressable>
+            <Pressable style={styles.row} onPress={() => setTheme("lightsOut")}>
+              <Text style={styles.subtitle}>Генеральная уборка</Text>
+              {theme === "lightsOut" ? (
+                <AntDesign name="checkcircle" size={24} color="#4A98E9" />
+              ) : (
+                <Entypo name="circle" size={24} color="#56636F" />
+              )}
+            </Pressable>
+          </View>
+        </BottomSheetModal>
+      </View>
+    </BottomSheetModalProvider>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    backgroundColor:'#111',
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: {
+    flex: 1,
+    backgroundColor: "gray",
+    alignItems: "center",
+    justifyContent: "center",
   },
-})
+  contentContainer: {
+    flex: 1,
+    alignItems: "center",
+    paddingHorizontal: 15,
+  },
+  row: {
+    width: "100%",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginVertical: 10,
+  },
+  title: {
+    fontWeight: "900",
+    letterSpacing: 0.5,
+    fontSize: 16,
+  },
+  subtitle: {
+    color: "#101318",
+    fontSize: 14,
+    fontWeight: "bold",
+  },
+  description: {
+    color: "#56636F",
+    fontSize: 13,
+    fontWeight: "normal",
+    width: "100%",
+  },
+});
